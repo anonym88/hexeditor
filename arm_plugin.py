b8a76e13134be86d7432437618c4673ba558fe53
iblaauw <naibla8358@gmail.com>
2016-01-12 19:10:04 -0500

    Added a BitString class. Started actually checking bits.

--- arm_plugin.py
+++ arm_plugin.py
@@ -1,10 +1,8 @@
 def process(token):
     bits = to_bitlist(token)
-    cmd1 = bits[:32]
-    cmd2 = bits[32:]
+    cmd1 = BitString(bits[:32])
+    cmd2 = BitString(bits[32:])
     return [ do_process(cmd1), do_process(cmd2) ]
-
-
 
 def byteToBits(byte):
     for i in xrange(8):
@@ -18,23 +16,67 @@ def to_bitlist(token):
         for bit in byteToBits(byte):
             bits.append(bit)
 
-    #[ bit for bit in [ byteToBits(byte) for byte in byteList ] ]
     return bits
 
-def bitsToStr(bits):
-    rev = reversed(bits)
-    ones = map(str, rev)
-    return ''.join(ones)
 
-def do_process(bits):
+class BitString(object):
+    def __init__(self, bits):
+        self.bits = bits
+
+    def __iter__(self):
+        return iter(self.bits)
+
+    def __len__(self):
+        return len(self.bits)
+
+    def __getitem__(self, indices):
+        if isinstance(indices, slice):
+            new_bits = self.bits[indices]
+        else:
+            new_bits = [ self.bits[indices] ]
+        return BitString(new_bits)
+
+    def __str__(self):
+        rev = reversed(self.bits)
+        ones = map(str, rev)
+        return ''.join(ones)
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            if len(other) != len(self):
+                return False
+            rev = reversed(other)
+            for b,s in zip(self.bits, rev):
+                x0 = b == 0 and s == '0'
+                x1 = b == 1 and s == '1'
+                val = x0 or x1
+                if not val: return False
+            return True
+
+        return NotImplemented
+
+
+def getBitString(bits):
     assert(len(bits) == 32)
 
     indices = range(8, 33, 8)
     bts = [ bits[x-8:x] for x in indices ]
-    ones = map(bitsToStr, bts)
+    ones = map(str, bts)
     return ' '.join(ones)
 
-    xx = reversed(bits[:8])
-    xx = map(str, xx)
-    return ''.join(xx)
+
+
+
+def do_process(bits):
+    assert(len(bits) == 32)
+
+    if bits[25:28] == "101":
+        return "Branch"
+    if bits[25:28] == "000":
+        return "Proc Data"
+    if bits[26:28] == "01":
+        return "Memory"
+
+    return getBitString(bits)
+
 
